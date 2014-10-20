fe2b9b19a7499c381fc067ea9efec7d06e6a44d5
Amit Hetawal <amit.hetawal@gmail.com>
2014-10-20 00:11:33 -0700

    Facebook Integration
    Updated code with FB infer for user and friends list

--- src/com/stealthecheese/activity/MainActivity.java
+++ src/com/stealthecheese/activity/MainActivity.java
@@ -1,37 +1,151 @@
 package com.stealthecheese.activity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 
-import com.example.stealthecheese.R;
-import com.stealthecheese.model.Transaction;
-import com.stealthecheese.model.UnitOfWork;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.model.GraphUser;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+import com.stealthecheese.R;
+import com.stealthecheese.application.StealTheCheeseApplication;
 
 
 public class MainActivity extends ActionBarActivity {
-
+	
+	private Button loginButton;
+	private Button logoutButton;
+	
 	@Override 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
 		
 		//Test parse code
-		UnitOfWork uow = new UnitOfWork();
-		Transaction newActivity = new Transaction("testFromuserId", "testToUserId", 5);
-		uow.activityDAO.createTransaction(newActivity);
+		//UnitOfWork uow = new UnitOfWork();
+		//Transaction newActivity = new Transaction("testFromuserId", "testToUserId", 5);
+		//uow.activityDAO.createTransaction(newActivity);
+		
+		loginButton = (Button) findViewById(R.id.loginButton);
+		loginButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onLoginButtonClicked();
+			}
+		});
+		
+		logoutButton = (Button) findViewById(R.id.logoutButton);
+		logoutButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				ParseFacebookUtils.getSession().closeAndClearTokenInformation();
+				
+				/*
+				 *  Do we need to delete the current user from parse, in order to start fresh 
+				 *  or just log them out and only update friend list when they log back in again
+				 */
+				ParseUser currentUser = ParseUser.getCurrentUser(); 
+				currentUser.deleteInBackground();
+				ParseUser.logOut();
+				
+			}
+		});
+		
+		
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if ((currentUser != null) && ParseFacebookUtils.isLinked(currentUser)) {
+			// Go to the user info activity
+			//showUserDetailsActivity();
+		}else {
+			//onLoginButtonClicked();
+		}
+		
+		
 	}
 
+	private void onLoginButtonClicked() {
+
+		List<String> permissions = Arrays.asList("public_profile", "user_friends");
+		ParseFacebookUtils.logIn(permissions, this, new LogInCallback() {
+			@Override
+			public void done(ParseUser user, ParseException err) {
+
+				if (user == null) {
+					Log.i(StealTheCheeseApplication.LOG_TAG,
+							"Uh oh. The user cancelled the Facebook login.");
+				} else if (user.isNew()) {
+					Log.i(StealTheCheeseApplication.LOG_TAG,
+							"User signed up and logged in through Facebook!");
+					getFBUserInfo(user);
+				} else {
+					Log.i(StealTheCheeseApplication.LOG_TAG,
+							"User logged in through Facebook!");
+
+					//showUserDetailsActivity();
+				}
+			}
+		});
+
+	}
+	
+	
+	private void getFBUserInfo(final ParseUser loggedInUser) {
+		Request request = Request.newMeRequest(ParseFacebookUtils.getSession(),
+				new Request.GraphUserCallback() {
+			@Override
+			public void onCompleted(GraphUser user, Response response) {
+				loggedInUser.put("facebookId", user.getId());
+				loggedInUser.put("firstName", user.getFirstName());
+				loggedInUser.put("lastName", user.getLastName());
+				
+				// Use ProfilePictureView if needed for UI
+				loggedInUser.put("profilePicUrl", String.format(StealTheCheeseApplication.PROFILE_PIC_URL, user.getId()));
+				getAndSaveFBUserFriendsInfo(loggedInUser);
+			}
+		});
+		request.executeAsync();
+
+	}
+
+	private void getAndSaveFBUserFriendsInfo(final ParseUser loggedInUser) {
+
+		// Returns only the list of friends which use the app also
+		Request request = Request.newMyFriendsRequest(ParseFacebookUtils.getSession(),
+				new Request.GraphUserListCallback() {
+			@Override
+			public void onCompleted(List<GraphUser> friends, Response response) {
+				List<String> friendsList = new ArrayList<String>();
+				if(friends.size() > 0){
+					Log.i(StealTheCheeseApplication.LOG_TAG, "Friend list size: " + friends.size());
+					for(GraphUser friend : friends){
+						friendsList.add(friend.getId());
+					}
+
+				}
+				loggedInUser.addAllUnique("friends", friendsList);
+				
+				//TODO: Do we need the callback version of this save in case of new friends updated ??
+				loggedInUser.saveInBackground();
+			}
+
+		});
+		request.executeAsync();
+
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -51,22 +165,14 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
+	
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	  super.onActivityResult(requestCode, resultCode, data);
+	  ParseFacebookUtils.finishAuthentication(requestCode, resultCode, data);
 	}
-
+	
+	
+	
 }
--- src/com/stealthecheese/application/StealTheCheeseApplication.java
+++ src/com/stealthecheese/application/StealTheCheeseApplication.java
@@ -3,22 +3,33 @@ package com.stealthecheese.application;
 import android.app.Application;
 
 import com.parse.Parse;
-import com.parse.ParseACL;
-import com.parse.ParseUser;
+import com.parse.ParseFacebookUtils;
+import com.stealthecheese.R;
+
 
 public class StealTheCheeseApplication extends Application {
-
+	
+	public static final String LOG_TAG = "stealcheese";
+	public static final String PROFILE_PIC_URL = "https://graph.facebook.com/%s/picture";
+	
   @Override
   public void onCreate() {
     super.onCreate();
     // Add your initialization code here
-    Parse.initialize(this, "nNADkosy5X7RyeklWIDkBqdYdEkI0RGpklYvm456", "0K62bbYxCq5U6hHo2XcWJ5fxbxjdYOB606tT0xdM");
-    ParseUser.enableAutomaticUser();
-    ParseACL defaultACL = new ParseACL();
+    // Parse.initialize(this, "nNADkosy5X7RyeklWIDkBqdYdEkI0RGpklYvm456", "0K62bbYxCq5U6hHo2XcWJ5fxbxjdYOB606tT0xdM");
+    
+    // (Amits ) Add your initialization code here
+    Parse.initialize(this, "He3esVXQ8BlaHA5ArPFQlM7YLvzDfgjpnL9INv0C", "zV6XlXFZ1AhqPUtiDveOUhjN5gZM11zG68qazbEr");
+
+    
+   // ParseUser.enableAutomaticUser();
+    //ParseACL defaultACL = new ParseACL();
       
     // If you would like all objects to be private by default, remove this line.
-    defaultACL.setPublicReadAccess(true);
+    //defaultACL.setPublicReadAccess(true);
     
-    ParseACL.setDefaultACL(defaultACL, true);
+    //ParseACL.setDefaultACL(defaultACL, true);
+    
+    ParseFacebookUtils.initialize(getString(R.string.facebook_app_id));
   }
 }
